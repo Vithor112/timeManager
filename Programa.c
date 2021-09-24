333524e987e1615632140a470480034df52659b6
Vithor <vithorpileco@gmail.com>
2021-09-24 13:56:55 -0300

    Adicionei arquivo C com a descrição do meu problema

---
+++ Programa.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*Problema: É que o site de bate o ponto não registra o horário que eu já trabalhei. Então eu quero um programa que cuide do horário que eu já trabalhei e receba meus pontos de entrada e saída.
+Continue rodando até fechar 6 horas */
+
+int main(void){
+
+
+
+
+    return 0;
+}
